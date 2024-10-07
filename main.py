67590adf5786abff235466508107ca249b1e7da4
deloygintaras <turcinasgintaras@gmail.com>
2024-10-07 14:17:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+# 1.Sukurkite ciklą kuris atspausdintų 10 kartų žodį “labas”.
+# for sk in range(10):
+#     print("labas")
+
+# 2. Sukurkite ciklą kuris atspausdintų skaičius nuo 0 iki 9.
+# for sk in range(0, 9):
+#     print(sk)
+
+
+# 3. Sukurkite masyvą iš dešimties augalų pavadinimų.
+# augalai = [
+# "Roze",
+# "Palmes",
+# "Abrikosai",
+# "Acenos",
+# "Adonis",
+# "Afelandra",
+# "Agarai",
+# "Agrastas",
+# "Aguonaiciai",
+# "Agurotis"
+# ]
+# print(augalai)
+
+
+
+# 4. Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje.
+# augalai = [
+# "Roze",
+# "Palmes",
+# "Abrikosai",
+# "Acenos",
+# "Adonis",
+# "Afelandra",
+# "Agarai",
+# "Agrastas",
+# "Aguonaiciai",
+# "Agurotis"
+# ]
+# for augalas in augalai:
+#     print(augalas)
+
+
+
+# 5. Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju. (atvirkščias ciklas).
+# augalai = [
+# "Roze",
+# "Palmes",
+# "Abrikosai",
+# "Acenos",
+# "Adonis",
+# "Afelandra",
+# "Agarai",
+# "Agrastas",
+# "Aguonaiciai",
+# "Agurotis"
+# ]
+# for augalas in reversed(augalai):
+#     print(augalas)
+
+#
+# # 6. Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
+# for sk in range(10, 50, 2):
+#     print(sk)
+
+
+# 7. Atspausdinkite kas antrą skaičių nuo 10 iki 50. (porinius)
+# Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite.
+# ( naudokite continue.) (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+
+
+
+# 8. Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
+
+
+
+# 9. Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. (du skaičiavimai)
+
+
+
+# 10. Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių  nei 10 simboliai. (tarp 5 ir 10 simbolių ilgio)
+
+
+
